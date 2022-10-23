b21f3c878c42ec60f2bee91e839bc4b3240e5d27
abhay-og <102411194+abhay-og@users.noreply.github.com>
2022-10-23 22:57:51 +0530

    Delete processor directory

--- processor/And.v
+++
@@ -1,5 +0,0 @@
-module And(input a,
-           input b,
-           output y);
-assign y = a&b;
-endmodule
--- processor/initialize_stall.v
+++
@@ -1,5 +0,0 @@
-module initialize_stall(input[31 : 0] PC, inout stall1);
-
-    assign stall1 = (PC==32'b0)? (1'b0):(stall1);
-
-endmodule
